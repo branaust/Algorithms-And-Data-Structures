1ecf0256a0f83db2905b470e5872dc7b3a94b85f
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-03-21 11:28:49 -0700

    fin

--- ProblemSolvingPatterns/CountUniqueValues.js
+++ ProblemSolvingPatterns/CountUniqueValues.js
@@ -1,1 +1,19 @@
-console.log('hello')
+const uniqueVals = (arr) => {
+    // INPUT: sorted array
+    // if i !==j i++, j++
+    // if i === j i++, i=j
+    i = 0
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1
+
+    // OUTPUT: number of unique values
+}
+
+let arr = [0, 1, 1, 2, 2, 3, 4, 4, 5, 6, 7]
+
+console.log(uniqueVals(arr))
