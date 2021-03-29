83b3d471af0fa9ce6a2bf83156451ded816e4c41
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-03-29 16:49:19 -0700

    BubbleSort

--- Searching Algorithms/BubbleSort.js
+++ Searching Algorithms/BubbleSort.js
@@ -1,15 +1,20 @@
 const bubbleSort = (arr) => {
+    let noSwaps
     for (let i = arr.length; i > 0; i--) {
+        noSwaps = true
         for (let j = 0; j < i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
+                noSwaps = false
+                console.log(arr)
             }
         }
+        if (noSwaps) break
     }
     return arr
 }
 
-console.log(bubbleSort([6, 12, 4, 1, 7]))
+console.log(bubbleSort([1, 2, 3, 4, 2]))
 
