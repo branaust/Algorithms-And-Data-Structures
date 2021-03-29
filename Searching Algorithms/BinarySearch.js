96e0521a5f71e5352c16c970e494a1b4ddb64688
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-03-29 05:30:24 -0700

    Binary Search

--- Searching Algorithms/BinarySearch.js
+++ Searching Algorithms/BinarySearch.js
@@ -1,17 +1,20 @@
 const binarySearch = (arr, target) => {
     let left = 0
     let right = arr.length - 1
-    let middle = (left + right) / 2
+    let middle = Math.round((left + right) / 2)
 
-    if (!middle.isInteger) {
-        middle = Math.round(middle)
+    while (arr[middle] !== target && left <= right) {
+        if (target < arr[middle]) {
+            right = middle - 1
+        }
+        if (target > arr[middle]) {
+            left = middle + 1
+        }
+        middle = Math.round((left + right) / 2)
     }
-    if (target === middle) return middle
-    if (target < middle) {
-        right = middle - 1
+    if (arr[middle] === target) {
+        return middle
     }
-    if (target > middle) {
-        left = middle + 1
-    }
+    else { return -1 }
 }
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7))
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 5))
