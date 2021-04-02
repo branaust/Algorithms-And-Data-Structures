5c21566fafa11bc3ab8e3e6fc5bd9b576e4155c9
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-04-02 09:34:13 -0700

    MergeSort

--- Searching Algorithms/MergeSort.js
+++ Searching Algorithms/MergeSort.js
@@ -2,7 +2,6 @@ const merge = (arr1, arr2) => {
     let sorted = []
     let i = 0
     let j = 0
-    console.log(arr1.length)
 
     while (i < arr1.length && j < arr2.length) {
         if (arr1[i] < arr2[j]) {
@@ -26,4 +25,14 @@ const merge = (arr1, arr2) => {
     return sorted
 }
 
-console.log(mergeSort([2, 4, 6, 8, 9, 10], [1, 3, 5, 7]))
+
+
+const mergeSort = (arr) => {
+    if (arr.length <= 1) return arr
+    let mid = Math.floor(arr.length / 2)
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+    return merge(left, right)
+}
+
+console.log(mergeSort([1000, 444, 555, 222, 666, 111, 8888, 333, 22, 55]))
