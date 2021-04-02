965ee8ac41effebf0d4c64dfb02e2d78b788102d
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-04-02 14:09:27 -0700

    updates

--- Searching Algorithms/RadixSort.js
+++ Searching Algorithms/RadixSort.js
@@ -1,8 +1,17 @@
-const getDigit = (num, place) => {
-    num = (("" + num).split(""))
-    let index = (num.length - 1) - place
-    return num[index]
+// const getDigit = (num, place) => {
+//     num = (("" + num).split(""))
+//     let index = (num.length - 1) - place
+//     return (num[index] * 1)
 
+// }
+
+const getDigit = (num, i) => {
+    return Math.floor(Math.abs(num) / Math.pow(10, i) % 10)
+}
+
+const digitCount = (num) => {
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1
 }
 
 console.log(getDigit(41275, 4))
