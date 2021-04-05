2e36753f40d8a8a3b7fb0789d63b34028642b2a3
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-04-05 07:01:50 -0700

    GET

--- Data Structures/SingleLinkedList.js
+++ Data Structures/SingleLinkedList.js
@@ -69,6 +69,20 @@ class SinglyLinkedList {
         return this
 
     }
+
+    get(index) {
+        if (index < 0 || index >= this.length) {
+            return null
+        }
+        let counter = 0
+        let current = this.head
+        while (counter < index) {
+            current = current.next
+            counter++
+        }
+        return current
+
+    }
 }
 
 let list = new SinglyLinkedList
@@ -76,4 +90,5 @@ list.push('HEY')
 list.push('Whatsup?')
 list.push('Not much you?')
 list.push('Not much you?')
-console.log(list.unshift('AYYAYY'))
+list.unshift('AYYAYY')
+console.log(list.get(1))
