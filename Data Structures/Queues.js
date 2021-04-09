f1d97a64d3b7e7891a22d457bd0b7dbd86616004
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-04-09 09:54:43 -0700

    QUEUES

---
+++ Data Structures/Queues.js
@@ -0,0 +1,40 @@
+// QUEUES - collection of data that abides by the FIFO principle - FIRST IN FIRST OUT
+
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null
+        this.last = null
+        this.length = 0
+    }
+
+    enqueue(val) {
+        let newNode = newNode(val)
+        if (!this.first) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            this.last.next = newNode
+            this.last = newNode
+        }
+        return this.length++
+    }
+
+    dequeue() {
+        if (!this.first) return null
+
+        let temp = this.first
+        if (this.first === this.last) {
+            this.last = null
+        }
+        this.first = this.first.next
+        this.length--
+        return temp
+    }
+}
