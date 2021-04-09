6c728cdeeb8fb1ee849684716149a963c17d9d73
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-04-09 06:57:50 -0700

    STACKS

---
+++ Data Structures/Stacks.js
@@ -0,0 +1,53 @@
+// Stacks - collection of data that abides by the LIFO principle - LAST IN FIRST OUT
+
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val)
+        if (!this.first) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            let next = this.first
+            this.first = newNode
+            newNode.next = next
+        }
+        this.size++
+        return this
+    }
+    pop() {
+        if (!this.first) {
+            return undefined
+        }
+        else {
+            let newFirst = this.first.next;
+            this.first = newFirst
+            this.size--
+            return this
+        }
+    }
+}
+
+const stack = new Stack()
+
+console.log(stack.push('One'))
+console.log(stack.push('Two'))
+console.log(stack.push('Three'))
+console.log(stack.push('Four'))
+console.log(stack.push('Five'))
+
+console.log(stack.pop())
+console.log(stack.pop())
+console.log(stack.pop())
