7e5d9010dc9521b7d95ea5c2ce234c24c70bfb2e
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-04-12 08:54:25 -0700

    Intro to TREES

---
+++ Data Structures/BinarySearchTrees.js
@@ -0,0 +1,8 @@
+// TREE - A tree is a data structure that consists of nodes in a Parent/Child relationship
+
+// ROOT - The top node in a tree
+// CHILD - A node directly connected to another node when moving away from the Root
+// Parent - The converse notion of a Child
+// Siblings - A group of nodes with the same Parent
+// Leaf - A node with no Children
+// Edge - The connection between one node and another
