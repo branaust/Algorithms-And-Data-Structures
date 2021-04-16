58774f67458347f4839e3b8427d451382aa0c141
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-04-16 07:14:21 -0700

    HashTables HASH FUNCTION

--- Data Structures/HashTables.js
+++ Data Structures/HashTables.js
@@ -1,3 +1,14 @@
 // HASH TABLES - A data structure that is used to store keys/value pairs.
 // It uses a hash function to compute an index into an array in which an element will be inserted
 
+const hash = (key, arrayLen) => {
+    let total = 0
+    for (let char of key) {
+        // map "a" to 1, "b" to 2
+        let value = char.charCodeAt(0) - 96
+        total = (total + value) % arrayLen;
+    }
+    return total
+}
+
+console.log(hash("yellow", 10))
