55567365b1eb008905a5583e5ec93d5f749ad3b9
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-04-21 08:59:06 -0700

    Hash Table Set and Get

--- Data Structures/HashTables.js
+++ Data Structures/HashTables.js
@@ -4,16 +4,33 @@
 // Seperate Chaining - at each index in our array we store values using a more sophisticated data structure (an array or linked list)
 // Linear Probing - when we find a collision, we search through the array to find the next empty slot
 
-const hash = (key, arrayLen) => {
-    let total = 0
-    let WEIRD_PRIME = 31
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
-        // map "a" to 1, "b" to 2
-        let char = key[i]
-        let value = char.charCodeAt(0) - 96
-        total = (total * WEIRD_PRIME + value) % arrayLen;
+// const hash = (key, arrayLen) => {
+//     let total = 0
+//     let WEIRD_PRIME = 31
+//     for (let i = 0; i < Math.min(key.length, 100); i++) {
+//         // map "a" to 1, "b" to 2
+//         let char = key[i]
+//         let value = char.charCodeAt(0) - 96
+//         total = (total * WEIRD_PRIME + value) % arrayLen;
+//     }
+//     return total
+// }
+
+class HashTable {
+    constructor(size = 53) {
+        this.keyMap = new Array(size);
     }
-    return total
+
+    _hash(key) {
+        let total = 0
+        let WEIRD_PRIME = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length
+        }
+        return total
+    }
 }
 
 console.log(hash("purples", 49))
