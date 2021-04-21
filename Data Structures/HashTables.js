6f586b7292284af10f69820510d1d8b00b8ff50d
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-04-21 06:51:21 -0700

    Handling Collisions

--- Data Structures/HashTables.js
+++ Data Structures/HashTables.js
@@ -1,6 +1,9 @@
 // HASH TABLES - A data structure that is used to store keys/value pairs.
 // It uses a hash function to compute an index into an array in which an element will be inserted
 
+// Seperate Chaining - at each index in our array we store values using a more sophisticated data structure (an array or linked list)
+// Linear Probing - when we find a collision, we search through the array to find the next empty slot
+
 const hash = (key, arrayLen) => {
     let total = 0
     let WEIRD_PRIME = 31
